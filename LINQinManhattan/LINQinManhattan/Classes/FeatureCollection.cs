d81de46e6036630911dd9c99398b3135221474f2
andrewquamme <andrewquamme@gmail.com>
2019-01-17 18:13:53 -0800

    creates classes for JSON deserializing

---
+++ LINQinManhattan/LINQinManhattan/Classes/FeatureCollection.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LINQinManhattan.Classes
+{
+    public partial class FeatureCollection
+    {
+        public string type { get; set; }
+        public List<Feature> features { get; set; }
+    }
+}
--- LINQinManhattan/LINQinManhattan/Program.cs
+++ LINQinManhattan/LINQinManhattan/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using LINQinManhattan.Classes;
 
 namespace LINQinManhattan
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string path = "../../../../../data.json";
+            FeatureCollection neighborhoods = ReadJSON(path);
+
+            var query =
+                from feature in neighborhoods.features
+                where feature.properties.neighborhood == "Chelsea"
+                select feature;
+
+            foreach (var feature in query)
+            {
+                Console.WriteLine($"Zipcode: {feature.properties.zip} \nCoordinates: {feature.geometry.coordinates[0]},{feature.geometry.coordinates[1]}");
+            }
         }
+
+        public static FeatureCollection ReadJSON(string path)
+        {
+            string json = File.ReadAllText(path);
+            return JsonConvert.DeserializeObject<FeatureCollection>(json);
+        }
+
+
+
+        
     }
 }
